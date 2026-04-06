cd865ea523a1603356b3254edd4aff535ccd402c
Gabriel <clinger.gab@gmail.com>
2026-04-05 22:55:24 -0700

    fix initialization scaling bug, and decouple ray from linet

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -5,12 +5,9 @@ Training utilities and loops for Multi-Stream Neural Networks.
 from .trainer import MultiStreamTrainer
 from .losses import FocalLoss, MultiStreamLoss
 from .optimizers import create_optimizer
-from .historical_median_stopping import HistoricalMedianStoppingRule
-
 __all__ = [
     'MultiStreamTrainer',
     'FocalLoss',
     'MultiStreamLoss',
     'create_optimizer',
-    'HistoricalMedianStoppingRule'
 ]
